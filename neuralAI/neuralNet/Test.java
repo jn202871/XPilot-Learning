cb17a8a9ed501d43f033be6a86985ffb15fbb885
jn202871 <jn202871@gmail.com>
2023-09-27 21:00:25 -0400

    fix

--- neuralAI/neuralNet/Test.java
+++ neuralAI/neuralNet/Test.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class test {
+public class Test {
 
 	public static void main(String[] args) {
 		double[][] tests = new double[][]{{0,0,0,0,0},{0,0,0,0,1},{0,0,0,1,0},{0,0,0,1,1},{0,0,1,0,0},{0,0,1,0,1},{0,0,1,1,0},
